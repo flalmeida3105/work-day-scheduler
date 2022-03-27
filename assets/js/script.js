0126b335084b028139fbb5d5926e51a02a5a65c3
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-27 02:05:30 -0400

    fixed grid columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,28 +17,42 @@ function createTimeRowElements() {
 
     for (var i = 0; i <= endTime; i++) {
         var timeFormElement = $("<form></form>")
-            .addClass("col-8 d-flex flex-wrap justify-content-center")
+            .addClass("col-10 d-flex flex-wrap justify-content-center")
 
         var timeBlockPElement = $("<p></p>")
             .addClass("time-block hour col-1")
-            .attr("id","hour-id-" + [i])
+            .css("text-align", "right")
             if (i <=3 ) {
                 currentTime = i + 9;
+                timeBlockPElement.attr("id", "hour-id-" + [currentTime])
                 timeBlockPElement.text([currentTime]+"AM");
             } else {
                 currentTime = i - 3;
+                timeBlockPElement.attr("id", "hour-id-" + [currentTime])
                 timeBlockPElement.text([currentTime]+ "PM");
             };
             timeFormElement.append(timeBlockPElement);
 
         var timeDescriptionElement = $("<input></input>")
             .addClass("description col-8")
-            .attr("id","description-id-" + [i]);
+            if (i <= 3) {
+                currentTime = i + 9;
+                timeDescriptionElement.attr("id", "description-id-" + [currentTime]);
+            } else {
+                currentTime = i - 3;
+                timeDescriptionElement.attr("id", "description-id-" + [currentTime]);
+            }
             timeFormElement.append(timeDescriptionElement);
 
         var timeButtonElement = $("<button></button>")
             .addClass("saveBtn col-1")
-            .attr("id","button-id-" + [i]);
+            if (i <= 3) {
+                currentTime = i + 9;
+                timeButtonElement.attr("id", "button-id-" + [currentTime]);
+            } else {
+                currentTime = i - 3;
+                timeButtonElement.attr("id", "button-id-" + [currentTime]);
+            }
             timeFormElement.append(timeButtonElement);
             timeContainerElement.append(timeFormElement);
 
