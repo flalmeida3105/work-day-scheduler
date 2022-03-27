0c9ae4c7849f022a2647fa959d179a350e82eab7
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-27 14:51:37 -0400

    fixed hour logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,26 @@
-var endTime = 9;
-var endTime24Hours = 18;
-
 function getCurrentDate() {
     // get current time
     var currentDate = moment().format("dddd, MMMM Do");
     return currentDate;
 };
 
-function getCurrentBusinessHour() {
+function getCurrentHour() {
     // get current business hours
-    var currentBusinessTime = moment().format("HH");
+    var currentBusinessTime = moment().format("H");
     return currentBusinessTime;
 };
 
-function getCurrentTime() {
-    for (var i = 9; i <= endTime24Hours; i++) {
-        time24Hours = i;
-        console.log(time24Hours);
-    }
-    return time24Hours;
-};
 
-
-function createTimeRowElements(currentBusinessTime, time24Hours) {
+function createTimeRowElements(currentHour, startTime, endTime) {
     var timeContainerElement = $(".row");
-
-    for (var i = 0; i <= endTime; i++) {
-        if (i <= 3) {
-            currentTime = i + 9;
-            currentTime = currentTime + "AM";
+    
+    var availableTime = ""
+    for (var i = startTime; i <= endTime; i++) {
+        if (i <= 12) {
+            availableTime = i + "AM";
         } else {
-            currentTime = i - 3;
-            currentTime = currentTime + "PM";
+            var newTime = i - 12;
+            availableTime = newTime + "PM";
         };
 
         var timeFormElement = $("<form></form>")
@@ -40,41 +29,37 @@ function createTimeRowElements(currentBusinessTime, time24Hours) {
         var timeBlockPElement = $("<p></p>")
             .addClass("time-block hour col-1")
             .css("text-align", "right")
-            timeBlockPElement.attr("id", "hour-id-" + [time24Hours])
-            timeBlockPElement.text([currentTime]);
+        timeBlockPElement.attr("id", "hour-id-" + availableTime)
+            timeBlockPElement.text([availableTime]);
             timeFormElement.append(timeBlockPElement);
 
         var timeDescriptionElement = $("<input></input>")
             .addClass("description col-8")
-            .attr("id", "description-id-" + [time24Hours]);
-            
-        console.log("Business TIme", currentBusinessTime, "Current 24 hours format", time24Hours);
-
-            currentBusinessTime = 11;
-             if (currentBusinessTime < time24Hours) {
+            .attr("id", "description-id-" + availableTime);
+        
+        if (currentHour == i) {
+                timeDescriptionElement.addClass("present");
+            } 
+        if (i < currentHour) {
                 timeDescriptionElement.addClass("past");
             } 
-            if (currentBusinessTime > time24Hours) {
+        if (i > currentHour) {
                 timeDescriptionElement.addClass("future");
-            } 
-            if (currentBusinessTime == time24Hours) {
-                timeDescriptionElement.addClass("present");
             }
 
             timeFormElement.append(timeDescriptionElement);
 
         var timeButtonElement = $("<button></button>")
             .addClass("saveBtn col-1")
-            .attr("id", "button-id-" + [time24Hours]);
+            .attr("id", "button-id-" + availableTime);
             timeFormElement.append(timeButtonElement);
             timeContainerElement.append(timeFormElement);
-
+        
     };
-    // var timeBlockPElement =  $("#hour-id")
-    console.log(timeContainerElement)
+        console.log(timeContainerElement)
 }
 
-// createTimeRowElements();
+
 
 function setCurrentDateTime(currentDate) { 
     // set current date and time in the jumbotron
@@ -86,9 +71,8 @@ function setCurrentDateTime(currentDate) {
 function startCalendar() {
     // call the calendar function 
     var currentDate = getCurrentDate();
-    var currentBusinessTime = getCurrentBusinessHour();
-    var time24Hours = getCurrentTime();
-    createTimeRowElements(currentBusinessTime, time24Hours);
+    var currentHour = getCurrentHour();
+    createTimeRowElements(currentHour, 9, 18);
     setCurrentDateTime(currentDate);
 };
 
