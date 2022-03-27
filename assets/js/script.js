5deb858d18c89d9c26a7b7f99d48e5f3c541bf50
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-27 01:48:14 -0400

    grid styles inprovements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-var startTime = 0;
-// var endTime = 9;
+var endTime = 9;
 
 function getCurrentDate() {
     // get current time
@@ -9,15 +8,46 @@ function getCurrentDate() {
 
 // function getCurrentBusinessHour() {
 //     // get current business hours
-//     var currentTime = moment().format("hA");
-//     return currentTime;
+//     var currentBusinessTime = moment().format("hA");
+//     return currentBusinessTime;
 // }
 
 function createTimeRowElements() {
-    var timeRowElements = $(".time-block");
+    var timeContainerElement = $(".row");
 
-    console.log(timeRowElements)
+    for (var i = 0; i <= endTime; i++) {
+        var timeFormElement = $("<form></form>")
+            .addClass("col-8 d-flex flex-wrap justify-content-center")
+
+        var timeBlockPElement = $("<p></p>")
+            .addClass("time-block hour col-1")
+            .attr("id","hour-id-" + [i])
+            if (i <=3 ) {
+                currentTime = i + 9;
+                timeBlockPElement.text([currentTime]+"AM");
+            } else {
+                currentTime = i - 3;
+                timeBlockPElement.text([currentTime]+ "PM");
+            };
+            timeFormElement.append(timeBlockPElement);
+
+        var timeDescriptionElement = $("<input></input>")
+            .addClass("description col-8")
+            .attr("id","description-id-" + [i]);
+            timeFormElement.append(timeDescriptionElement);
+
+        var timeButtonElement = $("<button></button>")
+            .addClass("saveBtn col-1")
+            .attr("id","button-id-" + [i]);
+            timeFormElement.append(timeButtonElement);
+            timeContainerElement.append(timeFormElement);
+
+    };
+    // var timeBlockPElement =  $("#hour-id")
+    console.log(timeContainerElement)
 }
+
+createTimeRowElements();
 
 function setCurrentDateTime(currentDate) { 
     // set current date and time in the jumbotron
