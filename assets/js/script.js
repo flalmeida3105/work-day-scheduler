d458a10b0ad9ad96061b04a275fb1b1e85f15f5b
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-26 23:41:05 -0400

    created initial grid design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
+var startTime = 0;
+// var endTime = 9;
+
 function getCurrentDate() {
     // get current time
     var currentDate = moment().format("dddd, MMMM Do");
     return currentDate;
 };
+
+// function getCurrentBusinessHour() {
+//     // get current business hours
+//     var currentTime = moment().format("hA");
+//     return currentTime;
+// }
+
+function createTimeRowElements() {
+    var timeRowElements = $(".time-block");
+
+    console.log(timeRowElements)
+}
 
 function setCurrentDateTime(currentDate) { 
     // set current date and time in the jumbotron
@@ -14,6 +29,8 @@ function setCurrentDateTime(currentDate) {
 function startCalendar() {
     // call the calendar function 
     var currentDate = getCurrentDate();
+    // var currentBusinessTime = getCurrentBusinessHour();
+    // createTimeRowElements(currentBusinessTime);
     setCurrentDateTime(currentDate);
 };
 
